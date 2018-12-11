a636437bd229ea15ef923300fa156465d8e7feaf
MarkusKurzmann <20150081@HTBLAPerg.onmicrosoft.com>
2018-12-11 10:22:16 +0100

    Changed stuff for auto-tests

--- OpenDrone-flight-controller/WifiHotspot/WifiHotspot.py
+++ OpenDrone-flight-controller/WifiHotspot/WifiHotspot.py
@@ -1,4 +1,3 @@
-import subprocess
 import os.path
 import xmltodict
 import json
@@ -11,27 +10,27 @@ def loadJSON():
         json_data=open('/etc/accesspoint/accesspoint.json').read()
         j = json.loads(json_data)
         name=''
-        password=''
+        pwd=''
         if 'accesspointName' in doc['opendrone']:
             name = doc['opendrone']['accesspointName']
-            if name==None:
+            if name is None:
                 name = j['ssid']
             
 
         if 'accesspointPassword' in doc['opendrone']:
-            password = doc['opendrone']['accesspointPassword']
-            if password==None:
-                password = j['password']
+            pwd = doc['opendrone']['accesspointPassword']
+            if pwd is None:
+                pwd = j['password']
 
         with open('/etc/accesspoint/accesspoint.json', 'w+') as f:
-                f.write('{"ssid": "'+str(name)+'", "inet": null, "wlan": "wlan0", "password": "'+str(password)+'", "netmask": "255.255.255.0", "ip": "192.168.1.254"}')
+                f.write('{"ssid": "'+str(name)+'", "inet": null, "wlan": "wlan0", "password": "'+str(pwd)+'", "netmask": "255.255.255.0", "ip": "192.168.1.254"}')
                 f.seek(0)
-    subprocess.call("sudo sudo pyaccesspoint --config start", shell=True)
+    os.system("sudo sudo pyaccesspoint --config start")
 
 if os.path.isfile('/etc/accesspoint/accesspoint.json'):
     print("started hotspot")
     loadJSON()
 else:
-    subprocess.call("sudo chmod u+x ./install.sh", shell=True)
-    subprocess.call("sudo bash ./install.sh", shell=True)
+    os.system("sudo bash ./install.sh")
+    print("started hotspot")
     loadJSON()
