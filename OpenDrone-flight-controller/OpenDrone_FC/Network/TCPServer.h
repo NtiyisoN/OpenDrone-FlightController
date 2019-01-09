252e13697164c401756dbebcc65bebd71e9c555c
MarkusKurzmann <20150081@HTBLAPerg.onmicrosoft.com>
2019-01-09 16:27:07 +0100

    Implemented such a beautiful server, you wont believe it

--- OpenDrone-flight-controller/OpenDrone_FC/Network/TCPServer.h
+++ OpenDrone-flight-controller/OpenDrone_FC/Network/TCPServer.h
@@ -1,47 +1,11 @@
-/*
- * Copyright (c) OpenDrone, 2018.  All rights reserved.
- * The entire project (including this file) is licensed under the GNU GPL v3.0
- */
-
-#ifndef TCP_SERVER_H
-#define TCP_SERVER_H
-
-#include <iostream>
-#include <vector>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-
-using namespace std;
-
-#define MAXPACKETSIZE 4096
-
+#pragma once
 class TCPServer
 {
 public:
-	int sockfd, newsockfd, n, pid;
-	struct sockaddr_in serverAddress;
-	struct sockaddr_in clientAddress;
-	pthread_t serverThread;
-	char msg[MAXPACKETSIZE];
-	static string Message;
-
-	void setup(int port);
-	string receive();
-	string getMessage();
-	void Send(string msg);
-	void detach();
-	void clean();
-
-private:
-	static void * Task(void * argv);
+    TCPServer();
+    ~TCPServer();
+    void startUp();
+    void acceptClients();
+    int sendMessage(int sd, char* msg);
 };
 
-#endif
