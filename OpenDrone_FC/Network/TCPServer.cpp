3454766091c6d82b75cc4ba0d7f6c5864d64ef9a
MarkusKurzmann <20150081@HTBLAPerg.onmicrosoft.com>
2019-01-29 08:54:23 +0100

    Fix complex method and some issues from WifiHotspot

--- OpenDrone_FC/Network/TCPServer.cpp
+++ OpenDrone_FC/Network/TCPServer.cpp
@@ -3,7 +3,7 @@
  * The entire project (including this file) is licensed under the GNU GPL v3.0
  * Purpose: TODO
  *
- * 	@author Tim Klecka
+ * 	@author Markus Kurzmann
  * 	@version 0.0.1 07.01.2019
  */
 
@@ -28,20 +28,6 @@
 #define PORT 2018  
 
 
-int opt = TRUE;
-int master_socket, addrlen, new_socket, client_socket[30],
-max_clients = 30, activity, i, valread, sd;
-int max_sd;
-struct sockaddr_in address;
-char buffer[1025];  //data buffer of 1K 
-
-char* Temp;
-
-//set of socket descriptors  
-fd_set readfds;
-
-char *message = "Hello Client...";
-
 TCPServer::TCPServer()
 {
 }
@@ -50,9 +36,10 @@ TCPServer::TCPServer()
 TCPServer::~TCPServer()
 {
 }
-
+struct sockaddr_in address;
 void TCPServer::startUp() {
-
+    isRunning = true;
+    
     //initialise all client_socket[] to 0 so not checked  
     for (i = 0; i < max_clients; i++)
     {
@@ -102,41 +89,35 @@ void TCPServer::startUp() {
 
 void TCPServer::acceptClients()
 {
-    while (TRUE)
+    fd_set readfds;
+    Modbus *mb = new Modbus();
+    while (isRunning)
     {
         //clear the socket set  
         FD_ZERO(&readfds);
-
         //add master socket to set  
         FD_SET(master_socket, &readfds);
         max_sd = master_socket;
-
         //add child sockets to set  
         for (i = 0; i < max_clients; i++)
         {
             //socket descriptor  
             sd = client_socket[i];
-
             //if valid socket descriptor then add to read list  
             if (sd > 0)
                 FD_SET(sd, &readfds);
-
             //highest file descriptor number, need it for the select function  
             if (sd > max_sd)
                 max_sd = sd;
         }
 
-        //wait for an activity on one of the sockets , timeout is NULL ,  
-        //so wait indefinitely  
+        //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely  
         activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-
         if ((activity < 0) && (errno != EINTR))
         {
             printf("select error");
         }
-
-        //If something happened on the master socket ,  
-        //then its an incoming connection  
+        //If something happened on the master socket , then its an incoming connection  
         if (FD_ISSET(master_socket, &readfds))
         {
             if ((new_socket = accept(master_socket,(struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
@@ -144,71 +125,12 @@ void TCPServer::acceptClients()
                 perror("accept");
                 exit(EXIT_FAILURE);
             }
-
-            //send new connection greeting message  
-            if (this->sendMessage(new_socket, message)==0)
-            {
-                perror("send");
-            }
-
-            puts("Welcome message sent successfully");
-
-
-            //add new socket to array of sockets  
-            for (i = 0; i < max_clients; i++)
-            {
-                //if position is empty  
-                if (client_socket[i] == 0)
-                {
-                    client_socket[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n", i);
-
-                    break;
-                }
-            }
+            this->addClient(new_socket);
         }
-        this->getTemp();
         valread = read(new_socket, buffer, 1024);
-        Modbus *mb = new Modbus();
         mb->Interpret(buffer);
-        //this->sendMessage(new_socket, Temp);
-        //printf("VALUE:%s\n", buffer);
         
-
-        //else its some IO operation on some other socket 
-        for (i = 0; i < max_clients; i++)
-        {
-            sd = client_socket[i];
-            if (FD_ISSET(sd, &readfds))
-            {
-                
-                //Check if it was for closing , and also read the  
-                //incoming message
-
-                if ((valread = read(sd, buffer, 1024)) == 0)
-                {
-                    //Somebody disconnected , get his details and print  
-                    getpeername(sd, (struct sockaddr*)&address, \
-                        (socklen_t*)&addrlen);
-                    printf("Host disconnected , ip %s , port %d \n",
-                        inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
-                    //Close the socket and mark as 0 in list for reuse  
-                    close(sd);
-                    client_socket[i] = 0;
-                }
-
-                //Echo back the message that came in  
-                else
-                {
-                    //set the string terminating NULL byte on the end  
-                    //of the data read  
-
-                    buffer[valread] = '\0';
-                    //this->sendMessage(sd, buffer);
-                }
-            }
-        }
+        this->checkIOOperation(readfds);
     }
 }
 
@@ -232,4 +154,55 @@ void TCPServer::getTemp() {
     Temp = (char*)(ss.str().c_str());
 
     this->sendMessage(new_socket, Temp);
+}
+
+void TCPServer::stopServer() 
+{
+    this->isRunning = false;
+}
+
+void TCPServer::startSendingValues() 
+{
+    while (isRunning) {
+        this->getTemp();
+        sleep(1);
+    }
+}
+
+void TCPServer::addClient(int new_sock) {
+    //add new socket to array of sockets  
+    for (int i = 0; i < max_clients; i++)
+    {
+        //if position is empty  
+        if (client_socket[i] == 0)
+        {
+            client_socket[i] = new_sock;
+            //printf("Adding to list of sockets as %d\n", i);
+            break;
+        }
+    }
+}
+
+void TCPServer::checkIOOperation(fd_set readfds) {
+    //else its some IO operation on some other socket 
+    for (i = 0; i < max_clients; i++)
+    {
+        sd = client_socket[i];
+        if (FD_ISSET(sd, &readfds))
+        {
+            //Check if it was for closing , and also read the incoming message
+            if ((valread = read(sd, buffer, 1024)) == 0)
+            {
+                //Somebody disconnected , get his details and print  
+                getpeername(sd, (struct sockaddr*)&address, \
+                    (socklen_t*)&addrlen);
+                printf("Host disconnected , ip %s , port %d \n",
+                    inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+                //Close the socket and mark as 0 in list for reuse  
+                close(sd);
+                client_socket[i] = 0;
+            }
+        }
+    }
 }
--- OpenDrone_FC/Network/TCPServer.h
+++ OpenDrone_FC/Network/TCPServer.h
@@ -4,12 +4,25 @@
  * The entire project (including this file) is licensed under the GNU GPL v3.0
  * Purpose: TODO
  *
- * 	@author Tim Klecka
+ * 	@author Markus Kurzmann
  * 	@version 0.0.1 07.01.2019
  */
 
+#include <sys/time.h> 
+
 class TCPServer
 {
+private:
+    int opt = 1;
+    int master_socket, addrlen, new_socket, activity, i, valread, sd;
+    int max_sd;
+    
+    char buffer[1025];  //data buffer of 1K 
+    bool isRunning = false;
+    char* Temp;
+    char *message = "Hello Client...";
+    void addClient(int new_sock);
+    void checkIOOperation(fd_set readfds);
 public:
     TCPServer();
     ~TCPServer();
@@ -17,5 +30,8 @@ public:
     void acceptClients();
     int sendMessage(int sd, char* msg);
     void getTemp();
-};
+    void stopServer();
+    void startSendingValues();
 
+    int client_socket[30], max_clients = 30;
+};
