d2369588dd1c82eddb92c4e0ddab5032ef2afbc3
tklecka <tim@klecka.at>
2019-02-19 13:18:29 +0100

    Fly without D in PID

--- OpenDrone_FC/Controller/PID.cpp
+++ OpenDrone_FC/Controller/PID.cpp
@@ -88,6 +88,7 @@ void PID::calcPid() {
 	else if (pid_i_mem_roll < pid_max_roll * -1)pid_i_mem_roll = pid_max_roll * -1;
 
 	pid_output_roll = pid_p_gain_roll * pid_error_temp + pid_i_mem_roll + pid_d_gain_roll * (pid_error_temp - pid_last_roll_d_error);
+	pid_output_roll = pid_p_gain_roll * pid_error_temp + pid_i_mem_roll /*+ pid_d_gain_roll * (pid_error_temp - pid_last_roll_d_error)*/;
 	if (pid_output_roll > pid_max_roll)pid_output_roll = pid_max_roll;
 	else if (pid_output_roll < pid_max_roll * -1)pid_output_roll = pid_max_roll * -1;
 
@@ -99,7 +100,8 @@ void PID::calcPid() {
 	if (pid_i_mem_pitch > pid_max_pitch)pid_i_mem_pitch = pid_max_pitch;
 	else if (pid_i_mem_pitch < pid_max_pitch * -1)pid_i_mem_pitch = pid_max_pitch * -1;
 
-	pid_output_pitch = pid_p_gain_pitch * pid_error_temp + pid_i_mem_pitch + pid_d_gain_pitch * (pid_error_temp - pid_last_pitch_d_error);
+	//pid_output_pitch = pid_p_gain_pitch * pid_error_temp + pid_i_mem_pitch + pid_d_gain_pitch * (pid_error_temp - pid_last_pitch_d_error);
+	pid_output_pitch = pid_p_gain_pitch * pid_error_temp + pid_i_mem_pitch /*+ pid_d_gain_pitch * (pid_error_temp - pid_last_pitch_d_error)*/;
 	if (pid_output_pitch > pid_max_pitch)pid_output_pitch = pid_max_pitch;
 	else if (pid_output_pitch < pid_max_pitch * -1)pid_output_pitch = pid_max_pitch * -1;
 
@@ -111,7 +113,8 @@ void PID::calcPid() {
 	if (pid_i_mem_yaw > pid_max_yaw)pid_i_mem_yaw = pid_max_yaw;
 	else if (pid_i_mem_yaw < pid_max_yaw * -1)pid_i_mem_yaw = pid_max_yaw * -1;
 
-	pid_output_yaw = pid_p_gain_yaw * pid_error_temp + pid_i_mem_yaw + pid_d_gain_yaw * (pid_error_temp - pid_last_yaw_d_error);
+	//pid_output_yaw = pid_p_gain_yaw * pid_error_temp + pid_i_mem_yaw + pid_d_gain_yaw * (pid_error_temp - pid_last_yaw_d_error);
+	pid_output_yaw = pid_p_gain_yaw * pid_error_temp + pid_i_mem_yaw /*+ pid_d_gain_yaw * (pid_error_temp - pid_last_yaw_d_error)*/;
 	if (pid_output_yaw > pid_max_yaw)pid_output_yaw = pid_max_yaw;
 	else if (pid_output_yaw < pid_max_yaw * -1)pid_output_yaw = pid_max_yaw * -1;
 
